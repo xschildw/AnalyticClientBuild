49121cdafbedf2339b23f84ec91ac60094480fdb
brucehoff <bruce.hoff@sagebase.org>
2014-10-02 11:45:12 -0700

    recursive upload to S3

--- rClientBuild/Jekins/R/updateLatestVersion.R
+++ rClientBuild/Jekins/R/updateLatestVersion.R
@@ -24,17 +24,18 @@ updateLatestVersion<-function(versionsEndpoint, awsAccessKeyId, secretAccessKey)
   }
   # now upload to S3
   bucket <- versionsEndpoint
-  uploadToS3File(toJSON(fileContent), bucket, targetFileName, awsAccessKeyId, secretAccessKey)
+  uploadToS3File(toJSON(fileContent), "application/json", bucket, targetFileName, awsAccessKeyId, secretAccessKey)
 }
 
-uploadToS3File<-function(content, bucket, targetFileName, awsAccessKeyId, secretAccessKey) {
+# uploads file content to S3
+uploadToS3File<-function(content, contentType, bucket, targetFileName, awsAccessKeyId, secretAccessKey) {
   #UTC timestamp "yyyy-MM-dd'T'HH:mm:ss.SSS'Z'" a bit in the future (e.g. an hour from now)
   futureOffsetSeconds <- 100
   expirationTimestamp <- sprintf("%sZ", format(Sys.time()+futureOffsetSeconds,"%Y-%m-%dT%H:%M:%OS2", tz="UTC"))
   # this is a hack. drop leap seconds
   expirationTimestamp <- gsub(":6[01][\\.]", ":59.", expirationTimestamp)
-  policyRaw <- sprintf("{\"expiration\": \"%s\",\"conditions\": [{\"bucket\": \"%s\" },{\"acl\": \"public-read\" },[\"eq\",\"$key\", \"%s\"],[\"content-length-range\", 0, \"10000\"],[\"starts-with\", \"$Content-Type\", \"application/json\"],]}",
-    expirationTimestamp, bucket, targetFileName, bucket, targetFileName)
+  policyRaw <- sprintf("{\"expiration\": \"%s\",\"conditions\": [{\"bucket\": \"%s\" },{\"acl\": \"public-read\" },[\"eq\",\"$key\", \"%s\"],[\"content-length-range\", 0, \"10000\"],[\"starts-with\", \"$Content-Type\", \"%s\"],]}",
+    expirationTimestamp, bucket, targetFileName, contentType)
   policyBase64<-base64(policyRaw, encode=TRUE)[1]
   signatureRaw<-hmac(secretAccessKey, policyBase64, algo="sha1", raw=TRUE)
   signatureBase64 <- base64(signatureRaw, encode=TRUE)
@@ -46,7 +47,7 @@ uploadToS3File<-function(content, bucket, targetFileName, awsAccessKeyId, secret
     AWSAccessKeyId=awsAccessKeyId,
     Policy=policyBase64,
     Signature=signatureBase64,
-    "Content-Type"="application/json",
+    "Content-Type"=contentType,
     file=content)
   
   # if warnings are not suppressed prints "Found possible curl options in form parameters: file"
@@ -54,6 +55,91 @@ uploadToS3File<-function(content, bucket, targetFileName, awsAccessKeyId, secret
   options(warn=-1)
   postForm(uri, .params=postfields)
   options(oldWarn)
+}
+
+# recursively upload the contents of a folder
+# in the initial call 'root' is the root directory of interest, 
+# 'relativePath' is NULL, and mimTypeMap=createMimeTypeMap()
+uploadFolderToS3<-function(root, relativePath, bucket, awsAccessKeyId, secretAccessKey, mimeTypeMap) {
+  if (is.null(relativePath)) {
+    path<-root
+  } else {
+    path<-file.path(root, relativePath)
+  }
+  if (!file.exists(path)) stop(sprintf("%s does not exist.", path))
+  if (file.info(path)$isdir) {
+    dirContent<-list.files(path)
+    for (f in dirContent) {
+      if (is.null(relativePath)) {
+        subdir<-f
+      } else {
+        subdir<-file.path(relativePath, f)
+      }
+      uploadFolderToS3(root, subdir, bucket, awsAccessKeyId, secretAccessKey, mimeTypeMap)
+    }
+  } else {
+    # it's a file, so upload the content
+    connection<-file(path)
+    fileContent<-paste(readLines(connection), collapse="\n")
+    close(connection)
+    contentType<-"text/plain"
+    targetFileName<-relativePath
+    uploadToS3File(content, contentType, bucket, awsAccessKeyId, secretAccessKey, mimeTypeMap) 
+  }
+}
+
+#
+# getMimeTypeForFile
+# given a file name, determine the MIME type using the publicly available Apache mapping
+#
+
+# get the file's extension and then look up the MIME type, if there is no extension, apply a default
+getMimeTypeForFile<-function(fileName, mimeTypeMap) {
+  defaultMimeType<-"application/octet-stream"
+  extension<-getExtension(fileName)
+  if (nchar(extension)==0) return(defaultMimeType) # default mime type
+  mimeType<-mimeTypeMap[[extension]]
+  if (is.null(mimeType)) mimeType<-defaultMimeType
+  return(mimeType)
+}
+
+# get the substring following the LAST "." in fname, or "" if there is no "."
+# this code takes into account that there may be multiple "."s in fname
+getExtension<-function(fname) {
+  suffix<-fname
+  dot<-0
+  while (dot>=0) {
+    lastDot<-dot
+    suffix<-substr(suffix, dot+1, nchar(suffix))
+    dot<-regexpr(".", suffix, fixed=T)[[1]]
+    if (dot==0) stop("Illegal state, dot==0")
+  }
+  if (lastDot==0) {
+    return("")
+  } else {
+    return(tolower(suffix))
+  }
+}
+
+
+# read the Apache MIME type list from the 'net and build a map in memory
+createMimeTypeMap<-function() {
+  apacheMimeTypeURL<-"http://svn.apache.org/repos/asf/httpd/httpd/branches/2.0.x/docs/conf/mime.types";
+  parsedResult<-strsplit(getURLContent(apacheMimeTypeURL), "\n", fixed=TRUE)[[1]]
+  ans<-list()
+  for (line in parsedResult) {
+    # skip blank lines and comments
+    if (nchar(line)>0 && substr(line,1,1)!="#") {
+      tokens<-strsplit(line, "[ \t]")[[1]]
+      if (length(tokens)>1) {
+       mimeType<-tokens[1]
+       for (token in tokens[2:length(tokens)]) if (nchar(token)>0) ans[[token]]<-mimeType
+      }
+    }
+  }
+  # The online list does not contain a mime type for .R files
+  ans[['r']] <- 'text/x-r'
+  return(ans)
 }
 
 
